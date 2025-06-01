5a9ac8730da90808a391b791e700a79485665459
DeepakMehra53 <dmehra594@gmail.com>
2025-06-01 19:37:33 +0530

    remove middleware from root file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,30 +16,6 @@ const app = new Hono<{
 app.route('/api/v1/user',userRouter);
 app.route('/api/v1/blog',blogRouter);
 
-app.use('/api/v1/blog/*', async (c, next) => {
-  const header = c.req.header("authentication") || "";
-  const token = header.startsWith("Bearer") ? header.slice(7) : null;
-
-
-  if (!token) {
-    c.status(401);
-    return c.json({ error: "Token not provided" });
-  }
-  try {
-    const payload = await verify(token, c.env.JWT_SECRET)
-    if (payload?.id) {
-      await next()
-    } else {
-      c.status(403);
-      return c.json({ error: "Unauthorized" });
-    }
-  } catch {
-    c.status(403);
-    return c.json({ error: "Invalid token" });
-  }
-
-
-})
 
 
 
