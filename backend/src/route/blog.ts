1cd92fb75d63274d2c74f74b17d577bcba1615c0
DeepakMehra53 <dmehra594@gmail.com>
2025-06-01 17:39:47 +0530

    middleware Blog route

--- backend/src/route/blog.ts
+++ backend/src/route/blog.ts
@@ -1,17 +1,33 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { a } from '../../.wrangler/tmp/bundle-S1oNKg/middleware-loader.entry';
+import { decode, verify } from "hono/jwt";
+
 
 export const blogRouter = new Hono<{
     Bindings:{
         DATABASE_URL:string;
         JWT_SECRET:string;
+    },
+    Variables:{
+        userId:string;
     }
 }>();
 
-blogRouter.use("*/",(c,next)=>{
-    next();
+blogRouter.use("*/",async(c,next)=>{
+    const authHeader =  c.req.header("authorization") || "";
+    const token = authHeader.startsWith("Bearer ") ? authHeader.slice(7) : "";
+    try {
+        const user = await verify(token, c.env.JWT_SECRET);
+        if (user && user.id) {
+            c.set("userId", user.id);
+            return next();
+        } else {
+            return c.json({ error: "Unauthorized" }, 401);
+        }
+    } catch (error) {
+        return c.json({ error: "Invalid token" }, 403);
+    }
 })
 
 
@@ -22,14 +38,14 @@ blogRouter.post('/', async(c) => {
     const prisma = new PrismaClient({
         datasourceUrl:c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-
+    const   userId = c.get("userId")
     try {
         const blog = await prisma.post.create({
             data:{
                 title,
                 content,
                 published,
-                authorId:1
+                authorId:Number(userId)
             }
         })
     } catch (error) {
