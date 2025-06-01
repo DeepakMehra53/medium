efeea0766a638b9fe74a1abf8474dc55badaa48b
DeepakMehra53 <dmehra594@gmail.com>
2025-06-01 21:14:20 +0530

    test-jest-cases

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,4 @@
-import { Hono } from 'hono'
-import { userRouter } from './route/user';
-import { blogRouter } from './route/blog';
-
-
-
-
-const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: string
-    JWT_SECRET: string
-
-  }
-}>();
-
-app.route('/api/v1/user',userRouter);
-app.route('/api/v1/blog',blogRouter);
-
+import {app} from './app'
 
 
 
