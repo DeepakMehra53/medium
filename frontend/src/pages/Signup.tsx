6dd51abda54411e8752fff55b234637fe0b9023d
DeepakMehra53 <dmehra594@gmail.com>
2025-06-03 13:24:39 +0530

    writing signup auth frontend

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+
+import { Auth } from '../components/Auth'
+import { Quote } from '../components/Quote'
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='grid grid-cols-2'>
+      <div>
+          <Auth/>
+      </div>
+      <div className='invisible lg:visible'>
+
+      <Quote/>
+      </div>
+    </div>
   )
 }
