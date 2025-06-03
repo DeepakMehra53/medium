0aa50a96e8fed94d738f7c42aba69109e3577704
DeepakMehra53 <dmehra594@gmail.com>
2025-06-03 23:43:18 +0530

    Avatar update

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,10 +1,16 @@
 import { BlogCard } from "../components/BlogCard"
 
 export const Blogs =()=>{
-    return <div>
-        <BlogCard authorName={"deepak"}
-            title={"dawdad"}
-            content={"daca adidnw thwe  winw adadaw"}
-            publishedDate={"2nd fed 2024"}/>
-    </div>
+    return (
+      <div>
+        <BlogCard
+          authorName={"deepak"}
+          title={
+            "Hey, I’m a fellow W streamer who just started watching—keep growing and take care!"
+          }
+          content={"daca adidnw thwe  winw adadaw"}
+          publishedDate={"2nd fed 2024"}
+        />
+      </div>
+    );
 }
